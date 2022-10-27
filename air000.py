1bea81ec7deba3f4e687b775dcc950859ce2e97b
Yahnis <yahnis.allegret@yahoo.com>
2022-10-27 07:13:03 +0200

    air 00+01 et prepa 2

---
+++ air000.py
@@ -0,0 +1,38 @@
+'''
+Créez un programme qui découpe une chaîne de caractères en tableau (séparateurs : espaces, tabulations, retours à la ligne).
+
+Votre programme devra utiliser une fonction prototypée comme ceci :
+ma_fonction(string_à_couper, string_séparateur) { // syntaxe selon votre langage
+	# votre algorithme
+	return (tableau)
+}
+
+
+Exemples d’utilisation :
+$> python exo.py “Bonjour les gars”
+Bonjour
+les
+gars
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+
+'''
+import sys
+
+# Recuperation du data avec teste d'erreur si pas d'argument
+def input_data():
+    l = sys.argv[1:]
+    if len(l)<1:
+      print("erreur")
+      sys.exit()
+    return l
+
+#On fais une boucle for pour afficher le resultat: 
+def afficher_liste(list):
+    for i in list:
+        print(i)
+
+
+#appel des fonctions:
+l=input_data()
+afficher_liste(l)
---
+++ air002.py
@@ -0,0 +1,19 @@
+'''
+Créez un programme qui transforme un tableau de chaînes de caractères en une seule chaîne de caractères. Espacés d’un séparateur donné en dernier argument au programme.
+
+Utilisez une fonction de ce genre (selon votre langage) :
+ma_fonction(array_de_strings, separateur) {
+	# votre algorithme
+	return (string)
+}
+
+
+Exemples d’utilisation :
+$> python exo.py “je” “teste” “des” “trucs” “ “
+Je teste des trucs
+
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+
+'''
+
