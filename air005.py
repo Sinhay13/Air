a4c169bb572adcdb844d647aea71a122633662d5
Yahnis <yahnis.allegret@yahoo.com>
2022-11-01 13:02:31 +0100

    boulot sur 5

--- air005.py
+++ air005.py
@@ -16,3 +16,43 @@ L’opération à appliquer sera toujours le dernier élément.
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 '''
+
+
+import sys
+from this import d
+
+# Recuperation du data avec teste d'erreur si pas d'argument
+def input_data():
+    l = sys.argv[1:]
+    if len(l)<=1:
+      print("erreur")
+      sys.exit()
+    else:
+        return l
+
+# on recupere le data quon a besion dans les variable qu'on a besion:
+def prepa (data):
+    coef_ope=data[-1]
+    coef=coef_ope[0]
+    ope=coef_ope[1:]
+    del data[-1]
+    return data, coef, ope
+
+# on teste les error argument pour que ce sois  bien des int et on les transforme en int
+def test_argument(data,coef):
+    pass
+# on verifi l'operateur + on fais les calculs + on les stock dans une nouvelle liste
+def calculs (data, coef, ope):
+    pass
+# On imprime le resultat
+def resultat(data):
+    pass
+
+
+
+#Appel des fonctions:
+data=input_data ()
+data, ope, coef =prepa(data)
+data, coef=test_argument(data, coef)
+#datafinal=calculs(data, coef, ope)
+#resultat(datafinal)
