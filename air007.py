b6814ad56a041cf7ff05b828f4640ea5c9588da6
Yahnis <yahnis.allegret@yahoo.com>
2022-11-08 17:39:10 +0100

    fin 07 et start 08

--- air007.py
+++ air007.py
@@ -19,3 +19,55 @@ $> ruby exo.rb 10 20 30 40 50 60 70 90 33
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 '''
+
+import sys
+
+# Recuperation du data avec teste d'erreur si pas d'argument
+def input_data():
+    l = sys.argv[1:]
+    if len(l) <=1:
+      print("erreur")
+      sys.exit()
+    else:
+        return l
+	
+
+# teste de data pour savoir si c'est int et mettre en int
+def Int(l):
+	inInt=[]
+	try:
+		for i in l:
+			inInt.append(int(i))
+		return inInt
+	except:
+		print("erreur")
+		sys.exit()
+
+# On trie toute la nouvelle liste avec le classique tri a bulle:
+def tri_a_bule(l):
+    n = len(l)
+    # Traverser tous les éléments du tableau
+    for i in range(n):
+        for j in range(0, n-i-1):
+            # échanger si l'élément trouvé est plus grand que le suivant
+            if l[j] > l[j+1] :
+                l[j], l[j+1] = l[j+1], l[j]
+    return l
+
+
+# Affichage des resultats:
+def final (l):
+	inStr=[]
+	for i in l:
+		inStr.append(str(i))
+	reponse= " ".join(inStr)
+	print(reponse)
+
+# Appelle des fonctions:
+
+list1=input_data()
+list2=Int(list1)
+list3=tri_a_bule(list2)
+final(list3)
+
+
