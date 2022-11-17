0778937f01dd531ed7ae7245292fb8e216524825
Yahnis <yahnis.allegret@yahoo.com>
2022-11-17 16:58:34 +0100

    start 10

---
+++ air010.py
@@ -0,0 +1,14 @@
+'''
+Créez un programme qui affiche le contenu d’un fichier donné en argument.
+
+
+Exemples d’utilisation :
+$> cat a.txt
+Je danse le mia
+$> ruby exo.rb “a.txt”
+Je danse le mia
+
+
+Afficher error et quitter le programme en cas de problèmes d’arguments ou de fichier inaccessible.
+
+'''
