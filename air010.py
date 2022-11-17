16b3501a3407e016b94274e41c01105c4dd05487
Yahnis <yahnis.allegret@yahoo.com>
2022-11-17 17:16:45 +0100

    Ajour d'un fichier texte et fin de 10

--- air010.py
+++ air010.py
@@ -12,3 +12,30 @@ Je danse le mia
 Afficher error et quitter le programme en cas de problèmes d’arguments ou de fichier inaccessible.
 
 '''
+
+import sys
+
+# Recuperation du data avec teste d'erreur si pas d'argument
+def input_data():
+    l = sys.argv[1:]
+    if len(l) !=1:
+      print("erreur")
+      sys.exit()
+    else:
+        name=" ".join(l)
+        return name
+
+# Lecture et affichage du contenu fichier + test d'érreurs :
+def reader(name):
+    try:
+        fichier = open(name, "r")
+        print (fichier.read())
+        fichier.close()
+    except:
+        print("error")
+
+# Appels des fonctions: 
+name=input_data()
+reader(name)
+
+
