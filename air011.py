436db43779b14d83995298941f7ad69d56bc7101
Yahnis <yahnis.allegret@yahoo.com>
2022-11-17 17:18:17 +0100

    start 11

---
+++ air011.py
@@ -0,0 +1,16 @@
+'''
+Afficher un escalier constitué d’un caractère et d’un nombre d’étages donné en paramètre.
+
+
+Exemples d’utilisation :
+$> ruby exo.rb O 5
+    O
+   OOO
+  OOOOO
+ OOOOOOO
+OOOOOOOOO
+
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+
+'''
