91264971d5db3eb6fb4c21b830b0f2e6d32bdaae
Yahnis <yahnis.allegret@yahoo.com>
2022-11-18 16:43:07 +0100

    fin 11

--- air011.py
+++ air011.py
@@ -14,3 +14,37 @@ OOOOOOOOO
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 '''
+
+import sys
+
+# Recuperation du data avec teste d'erreur si pas d'argument
+def input_data():
+    l = sys.argv[1:]
+    if len(l) !=1:
+        print("erreur")
+        sys.exit()
+    else:
+        try:
+            n=" ".join(l)
+            n_int=int(n)
+            return n_int
+        except:
+            print("erreur")
+            sys.exit()
+
+# On fais la pyramide : 
+def pyramide(n):
+    m="O"
+    print(" "*(n+1)+m)
+    for x in range(n):
+        m+="OO"
+        print(' '*(n-x)+m)
+
+
+
+# Appel des fonctions: 
+n=input_data()
+pyramide(n)
+
+
+
