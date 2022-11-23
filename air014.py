af00fefee6cbe261fdef6fd133b82ceb13f0f881
Yahnis <yahnis.allegret@yahoo.com>
2022-11-23 10:40:45 +0100

    fin 14 + start 13

---
+++ air014.py
@@ -0,0 +1,14 @@
+'''
+Créez un programme qui célèbre votre victoire.
+
+
+Exemples d’utilisation :
+$> python exo.py
+J’ai terminé l’Épreuve de l’Air et c’était [].
+$>
+
+Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+
+'''
+
+print("J'ai terminer l'epreuve de l'air c'est facile seulement au debut")
